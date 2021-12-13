d731682085a2e6bfea21aac3b5d05baea6c08654
Igboanugwo Collins <munisco12@gmail.com>
2021-12-13 16:36:08 +0100

    working on plans

--- src/main/layout/home/Plans.js
+++ src/main/layout/home/Plans.js
@@ -9,6 +9,9 @@ function Plans() {
                     Our Investment Plans
                 </h1>
             </div>
+            <div className='plans__category'>
+                <h1>Daily investment packages</h1>
+            </div>
             <div className='plans'>
                 {' '}
                 <div className='plan'>
@@ -80,22 +83,26 @@ function Plans() {
                         INVEST
                     </Link>
                 </div>
+               
+            </div>
+            <div className='plans'>
+                {' '}
                 <div className='plan'>
-                    <h2 className='plan__title'>Master</h2>
+                    <h2 className='plan__title'>starter</h2>
                     <div className='plan__icon'>
                         <p className='plan__icon-percent'>
-                            6% <span className='plan__icon-span'>/daily</span>
+                            2% <span className='plan__icon-span'>/daily</span>
                         </p>
                     </div>
 
                     <ul className='plan__list'>
                         <li className='plan__list-item'>
-                            Minimum Investment: $50,000
+                            Minimum Investment: $300
                         </li>
                         <li className='plan__list-item'>
-                            Maximum Investment: $90,999
+                            Maximum Investment: $4,999
                         </li>
-                        <li className='plan__list-item'>6% daily profit</li>
+                        <li className='plan__list-item'>2% daily profit</li>
                         <li className='plan__list-item'>10% referral bonus</li>
                     </ul>
 
@@ -104,21 +111,21 @@ function Plans() {
                     </Link>
                 </div>
                 <div className='plan'>
-                    <h2 className='plan__title'>Diamond</h2>
+                    <h2 className='plan__title'>Amateur</h2>
                     <div className='plan__icon'>
                         <p className='plan__icon-percent'>
-                            10% <span className='plan__icon-span'>/daily</span>
+                            3% <span className='plan__icon-span'>/daily</span>
                         </p>
                     </div>
 
                     <ul className='plan__list'>
                         <li className='plan__list-item'>
-                            Minimum Investment: $100,000
+                            Minimum Investment: $5000
                         </li>
                         <li className='plan__list-item'>
-                            Maximum Investment: $500,000
+                            Maximum Investment: $9,999
                         </li>
-                        <li className='plan__list-item'>10% daily profit</li>
+                        <li className='plan__list-item'>3% daily profit</li>
                         <li className='plan__list-item'>10% referral bonus</li>
                     </ul>
 
@@ -126,6 +133,103 @@ function Plans() {
                         INVEST
                     </Link>
                 </div>
+                <div className='plan'>
+                    <h2 className='plan__title'>Professional</h2>
+                    <div className='plan__icon'>
+                        <p className='plan__icon-percent'>
+                            4% <span className='plan__icon-span'>/daily</span>
+                        </p>
+                    </div>
+
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            Minimum Investment: $10000
+                        </li>
+                        <li className='plan__list-item'>
+                            Maximum Investment: $49,999
+                        </li>
+                        <li className='plan__list-item'>4% daily profit</li>
+                        <li className='plan__list-item'>10% referral bonus</li>
+                    </ul>
+
+                    <Link className='plan__btn' to='Auth/signup'>
+                        INVEST
+                    </Link>
+                </div>
+               
+            </div>
+            <div className='plans'>
+                {' '}
+                <div className='plan'>
+                    <h2 className='plan__title'>starter</h2>
+                    <div className='plan__icon'>
+                        <p className='plan__icon-percent'>
+                            2% <span className='plan__icon-span'>/daily</span>
+                        </p>
+                    </div>
+
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            Minimum Investment: $300
+                        </li>
+                        <li className='plan__list-item'>
+                            Maximum Investment: $4,999
+                        </li>
+                        <li className='plan__list-item'>2% daily profit</li>
+                        <li className='plan__list-item'>10% referral bonus</li>
+                    </ul>
+
+                    <Link className='plan__btn' to='Auth/signup'>
+                        INVEST
+                    </Link>
+                </div>
+                <div className='plan'>
+                    <h2 className='plan__title'>Amateur</h2>
+                    <div className='plan__icon'>
+                        <p className='plan__icon-percent'>
+                            3% <span className='plan__icon-span'>/daily</span>
+                        </p>
+                    </div>
+
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            Minimum Investment: $5000
+                        </li>
+                        <li className='plan__list-item'>
+                            Maximum Investment: $9,999
+                        </li>
+                        <li className='plan__list-item'>3% daily profit</li>
+                        <li className='plan__list-item'>10% referral bonus</li>
+                    </ul>
+
+                    <Link className='plan__btn' to='Auth/signup'>
+                        INVEST
+                    </Link>
+                </div>
+                <div className='plan'>
+                    <h2 className='plan__title'>Professional</h2>
+                    <div className='plan__icon'>
+                        <p className='plan__icon-percent'>
+                            4% <span className='plan__icon-span'>/daily</span>
+                        </p>
+                    </div>
+
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            Minimum Investment: $10000
+                        </li>
+                        <li className='plan__list-item'>
+                            Maximum Investment: $49,999
+                        </li>
+                        <li className='plan__list-item'>4% daily profit</li>
+                        <li className='plan__list-item'>10% referral bonus</li>
+                    </ul>
+
+                    <Link className='plan__btn' to='Auth/signup'>
+                        INVEST
+                    </Link>
+                </div>
+               
             </div>
         </>
     )
