4cc0c660d409c7c3c38f76feb947a400f37dfd13
Igboanugwo Collins <munisco12@gmail.com>
2021-12-13 16:50:23 +0100

    working on plans

--- src/components/ReactParticles.js
+++ src/components/ReactParticles.js
@@ -5,56 +5,58 @@ import Particles from 'react-tsparticles'
 const ReactParticles = () => {
     
     return (
-        <div>
-            <Particles
-                id='tsparticles'
-                options={{
-                    fpsLimit: 60,
-                    particles: {
-                        color: {
-                            value: '#ffffff',
-                        },
-                        links: {
-                            color: '#ffffff',
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.2,
-                            width: 1,
-                        },
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            direction: 'none',
-                            enable: true,
-                            outMode: 'bounce',
-                            random: false,
-                            speed: 4,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                value_area: 800,
-                            },
-                            value: 30,
-                        },
-                        opacity: {
-                            value: 0.2,
-                        },
+        // <div>
+        //     <Particles
+        //         id='tsparticles'
+        //         options={{
+        //             fpsLimit: 60,
+        //             particles: {
+        //                 color: {
+        //                     value: '#ffffff',
+        //                 },
+        //                 links: {
+        //                     color: '#ffffff',
+        //                     distance: 150,
+        //                     enable: true,
+        //                     opacity: 0.2,
+        //                     width: 1,
+        //                 },
+        //                 collisions: {
+        //                     enable: true,
+        //                 },
+        //                 move: {
+        //                     direction: 'none',
+        //                     enable: true,
+        //                     outMode: 'bounce',
+        //                     random: false,
+        //                     speed: 4,
+        //                     straight: false,
+        //                 },
+        //                 number: {
+        //                     density: {
+        //                         enable: true,
+        //                         value_area: 800,
+        //                     },
+        //                     value: 30,
+        //                 },
+        //                 opacity: {
+        //                     value: 0.2,
+        //                 },
 
-                        shape: {
-                            type: '',
-                        },
-                        size: {
-                            random: true,
-                            value: 5,
-                        },
-                    },
-                    detectRetina: true,
-                }}
-            />
-        </div>
+        //                 shape: {
+        //                     type: '',
+        //                 },
+        //                 size: {
+        //                     random: true,
+        //                     value: 5,
+        //                 },
+        //             },
+        //             detectRetina: true,
+        //         }}
+        //     />
+        // </div>
+
+        <p>Hello world</p>
     )
 }
 
