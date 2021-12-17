694b5daebdcd8fe1e8a6c4deed949ed858d9a9e3
Igboanugwo Collins <munisco12@gmail.com>
2021-12-17 22:59:13 +0100

    working on how to buy bitcoin page

--- src/containers/HowToBuyBitcoin.js
+++ src/containers/HowToBuyBitcoin.js
@@ -12,13 +12,17 @@ function HowToBuyBitcoin() {
             <div className='about'>
                 <h2 className='heading-2 about__header'>HOW TO BUY BITCOIN</h2>
                 <div className='about__content'>
-                    <h3 className='heading-3 about__heading'>
-                        Welcome to fxcryptotokeninvestment
-                    </h3>
+                
+                    <p className='about__text'>How to use Bitcoin ATM: step by step</p>
                     <video width='320' height='240' controls>
                         <source src={vd1} type='video/mp4' />
                         Your browser does not support the video tag.
                     </video>{' '}
+                    <video width='320' height='240' controls>
+                        <source src={vd2} type='video/mp4' />
+                        Your browser does not support the video tag.
+                    </video>{' '}
+                    <p className='about__text'>How to buy Bitcoin on Cash App instantly</p>
                     <p className='about__text'>
                         fxcryptotokeninvestment.com is one of the largest crypto
                         investment. We have been servicing our clients since
@@ -28,26 +32,7 @@ function HowToBuyBitcoin() {
                         Norwegian Sea to the shores of Western Europe.
                     </p>
                     <h3 className='heading-3 about__heading'>Our Vision</h3>
-                    <p className='about__text'>
-                        Our vision is to continuously seek for ways in which to
-                        feature worth for our investors and make sure we tend to
-                        never lose sight of our company values and core vision.
-                        Our targeted senior management team is committed to
-                        making sure that our high standards are maintained, our
-                        regulation is strict and our complete support is given
-                        passion and energy.
-                    </p>
-                    <h3 className='heading-3 about__heading'>Our Mission</h3>
-                    <p className='about__text'>
-                        Our mission is to deliver to clients wonderful services
-                        through our hard working, innovatives services and full
-                        commitments to clients’ satisfaction. We have a tendency
-                        to apply modern fast technology and use a team of
-                        extremely experienced professionals to bring clients’
-                        monetary dreams in to real life. Our passion is to
-                        continually work hard to assist clients get the most
-                        effective potential value and cut price.
-                    </p>
+                   
                 </div>
 
                 <div className='about__cryptoMarket'>
