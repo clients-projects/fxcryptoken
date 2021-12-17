d1fb9a87bf368a5de06acd2092e4ab2b16492c5f
Igboanugwo Collins <munisco12@gmail.com>
2021-12-17 23:07:19 +0100

    working on how to buy bitcoin page

--- src/containers/HowToBuyBitcoin.js
+++ src/containers/HowToBuyBitcoin.js
@@ -3,6 +3,7 @@ import CryptoMarketWatch from '../tradeviewWidgets/CryptoMarketWatch'
 import ReactParticles from '../components/ReactParticles'
 import vd1 from '../videos/vd1.mp4'
 import vd2 from '../videos/vd2.mp4'
+import { Link } from 'react-router-dom'
 
 function HowToBuyBitcoin() {
     return (
@@ -12,8 +13,17 @@ function HowToBuyBitcoin() {
             <div className='about'>
                 <h2 className='heading-2 about__header'>HOW TO BUY BITCOIN</h2>
                 <div className='about__content'>
-                
-                    <p className='about__text'>How to use Bitcoin ATM: step by step</p>
+                    <h3 className='heading-3 about__heading'>
+                        Why these videos
+                    </h3>
+                    <p className='about__text'>
+                        As it is part of our mission to deliver to clients wonderful services
+                        through our hard working, innovating services and full
+                        commitments to clients’ satisfaction, we came up with these specific video guilds that will help you to buy bitcoins without any prior knowledge of the crypto world, Please try watching to the end of each video for a clear understanding, if you have any questions or suggestions please do well to<Link to='contact-us'>contact us</Link>
+                    </p>
+                    <p className='about__text'>
+                        How to use Bitcoin ATM: step by step
+                    </p>
                     <video width='320' height='240' controls>
                         <source src={vd1} type='video/mp4' />
                         Your browser does not support the video tag.
@@ -22,17 +32,9 @@ function HowToBuyBitcoin() {
                         <source src={vd2} type='video/mp4' />
                         Your browser does not support the video tag.
                     </video>{' '}
-                    <p className='about__text'>How to buy Bitcoin on Cash App instantly</p>
                     <p className='about__text'>
-                        fxcryptotokeninvestment.com is one of the largest crypto
-                        investment. We have been servicing our clients since
-                        2010 and we are expanding our market share till this
-                        day. Our exclusive location brings us an opportunity to
-                        work with clients anywhere from the coast of the
-                        Norwegian Sea to the shores of Western Europe.
+                        How to buy Bitcoin on Cash App instantly
                     </p>
-                    <h3 className='heading-3 about__heading'>Our Vision</h3>
-                   
                 </div>
 
                 <div className='about__cryptoMarket'>
