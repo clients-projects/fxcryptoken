a5f6866f1fd16a690276db6b07fd8b9478236dfa
Igboanugwo Collins <munisco12@gmail.com>
2022-01-07 08:07:29 +0100

    added profit and active deposit to the dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -10,7 +10,7 @@ import { dataPie, legendPie } from '../variables/Variables'
 import CrytoMarketWatch from '../tradeviewWidgets/CryptoMarketWatch'
 
 const Dashboard = (props) => {
-   const createLegend = (json) =>{
+    const createLegend = (json) => {
         var legend = []
         for (var i = 0; i < json['names'].length; i++) {
             var type = 'fa fa-circle text-' + json['types'][i]
@@ -23,116 +23,114 @@ const Dashboard = (props) => {
 
     //Have to fetch the user account balance
     const [userAccountBalance, setUserAccountBalance] = useState(0)
-    const [totalUserDeposits, setTotalUserDeposits] = useState(0)
-    const [totalUserWithdrawals, setTotalUserWithdrawals] = useState(0)
-    const [pendingDepositsCount, setPendingDepositsCount] = useState(0)
-    
-    useEffect(() => {
-        if(props.userData.hasOwnProperty('username')){
+    const [activeDeposit, setActiveDeposit] = useState(0)
+    const [profit, setProfit] = useState(0)
+    const [referralBonus, setReferralBonus] = useState(0)
+    const [totalEarnings, setTotalEarnings] = useState(0)
 
+    useEffect(() => {
+        if (props.userData.hasOwnProperty('username')) {
             let keepProfits = 0
 
             props.userDeposits.map((p) => {
-                keepProfits += p.profit
+                return (keepProfits += p.profit)
             })
 
-            const accountBalandProfits = props.userData.accountBalance + keepProfits
+            const accountBalandProfits =
+                props.userData.accountBalance + keepProfits
 
             setUserAccountBalance(accountBalandProfits)
+            setProfit(keepProfits)
+            setTotalEarnings(props.totalEarnings)
         }
 
-        if(props.totalUserDeposits){
-            setTotalUserDeposits(props.totalUserDeposits)
-            setTotalUserWithdrawals(props.totalUserWithdrawals)
-            setPendingDepositsCount(props.pendingDepositsCount)
+        if (props.userDeposits) {
+            setActiveDeposit(props.userData.accountBalance)
+            setReferralBonus(props.totalReferralCommission)
         }
     }, [props])
 
     const displayUserFunds = `$${userAccountBalance}`
-    const displayUserDeposits = `$${totalUserDeposits}`
-    const displayUserWithdrawals = `$${totalUserWithdrawals}`
+    const displayActiveDeposits = `$${activeDeposit}`
+    const displayProfit = `$${totalEarnings > 1 ? totalEarnings : profit}`
+    const displayReferralBonus = `$${referralBonus}`
 
-        return (
-            <div className='content'>
-                <Grid fluid>
-                    <Row>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={
-                                    <i className='pe-7s-server text-warning' />
-                                }
-                                statsText='Account Balance'
-                                statsValue= {displayUserFunds}
-                                statsIcon={<i className='fa fa-refresh' />}
-                                statsIconText='Updated now'
-                            />
-                        </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={
-                                    <i className='pe-7s-wallet text-success' />
-                                }
-                                statsText='Total Withdrawal'
-                                statsValue={displayUserWithdrawals}
-                                statsIcon={<i className='fa fa-calendar-o' />}
-                                statsIconText='Updated now'
-                            />
-                        </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={
-                                    <i className='pe-7s-graph1 text-danger' />
-                                }
-                                statsText='Total Deposit'
-                                statsValue={displayUserDeposits}
-                                statsIcon={<i className='fa fa-clock-o' />}
-                                statsIconText='Updated now'
-                            />
-                        </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={<i className='pe-7s-cash text-info' />}
-                                statsText='Pending Deposits'
-                                statsValue={pendingDepositsCount}
-                                statsIcon={<i className='fa fa-refresh' />}
-                                statsIconText='Updated now'
-                            />
-                        </Col>
-                    </Row>
-                    <Row style={{ marginTop: '4rem' }}>
-                        <Col md={8}>
-                            <div style={{ height: '70vh' }}>
-                                <CrytoMarketWatch />
-                            </div>
-                        </Col>
-                        <Col md={4}>
-                            <Card
-                                statsIcon='fa fa-clock-o'
-                                title='Transactions'
-                                category='investment activities'
-                                stats='Updated now'
-                                content={
-                                    <div
-                                        id='chartPreferences'
-                                        className='ct-chart ct-perfect-fourth'
-                                    >
-                                        <ChartistGraph
-                                            data={dataPie}
-                                            type='Pie'
-                                        />
-                                    </div>
-                                }
-                                legend={
-                                    <div className='legend'>
-                                        {createLegend(legendPie)}
-                                    </div>
-                                }
-                            />
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
-        )
+    return (
+        <div className='content'>
+            <Grid fluid>
+                <Row>
+                    <Col lg={3} sm={6}>
+                        <StatsCard
+                            bigIcon={
+                                <i className='pe-7s-server text-warning' />
+                            }
+                            statsText='Account Balance'
+                            statsValue={displayUserFunds}
+                            statsIcon={<i className='fa fa-refresh' />}
+                            statsIconText='Updated now'
+                        />
+                    </Col>
+                    <Col lg={3} sm={6}>
+                        <StatsCard
+                            bigIcon={
+                                <i className='pe-7s-wallet text-success' />
+                            }
+                            statsText='Active Deposit'
+                            statsValue={displayActiveDeposits}
+                            statsIcon={<i className='fa fa-calendar-o' />}
+                            statsIconText='Updated now'
+                        />
+                    </Col>
+                    <Col lg={3} sm={6}>
+                        <StatsCard
+                            bigIcon={<i className='pe-7s-graph1 text-danger' />}
+                            statsText='Total Profit'
+                            statsValue={displayProfit}
+                            statsIcon={<i className='fa fa-clock-o' />}
+                            statsIconText='Updated now'
+                        />
+                    </Col>
+                    <Col lg={3} sm={6}>
+                        <StatsCard
+                            bigIcon={<i className='pe-7s-cash text-info' />}
+                            statsText='Referral Bonus'
+                            statsValue={displayReferralBonus}
+                            statsIcon={<i className='fa fa-refresh' />}
+                            statsIconText='Updated now'
+                        />
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: '4rem' }}>
+                    <Col md={8}>
+                        <div style={{ height: '70vh' }}>
+                            <CrytoMarketWatch />
+                        </div>
+                    </Col>
+                    <Col md={4}>
+                        <Card
+                            statsIcon='fa fa-clock-o'
+                            title='Transactions'
+                            category='investment activities'
+                            stats='Updated now'
+                            content={
+                                <div
+                                    id='chartPreferences'
+                                    className='ct-chart ct-perfect-fourth'
+                                >
+                                    <ChartistGraph data={dataPie} type='Pie' />
+                                </div>
+                            }
+                            legend={
+                                <div className='legend'>
+                                    {createLegend(legendPie)}
+                                </div>
+                            }
+                        />
+                    </Col>
+                </Row>
+            </Grid>
+        </div>
+    )
 }
 
 const mapStateToProps = (state) => {
@@ -141,12 +139,10 @@ const mapStateToProps = (state) => {
         loading: state.user.loading,
         tokenId: state.auth.tokenId,
         userData: state.auth.userData,
-        userFundAccount: state.auth.userFundAccount,
         userId: state.auth.userId,
-        totalUserDeposits: state.auth.totalUserDeposits,
         userDeposits: state.auth.userDeposits,
-        totalUserWithdrawals: state.auth.totalUserWithdrawals,
-        pendingDepositsCount: state.auth.pendingDepositsCount,
+        totalReferralCommission: state.auth.userData.totalReferralCommission,
+        totalEarnings: state.auth.userData.totalEarnings,
     }
 }
 
